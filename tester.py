b5f2389c432851383cca4bfd503e65bd4a26a21b
justincuig <39042844+justincuig@users.noreply.github.com>
2023-11-20 22:15:58 -0800

    Update tester.py for project 4

--- tester.py
+++ tester.py
@@ -161,6 +161,16 @@ def generate_test_suite_v3():
         fails,
     )
 
+def generate_test_suite_v4():
+    """wrapper for generate_test_suite for v4"""
+    tests = __get_file_names(getcwd() + "/v4/tests/")
+    fails = __get_file_names(getcwd() + "/v4/fails/")
+    return __generate_test_suite(
+        4,
+        tests,
+        fails,
+    )
+
 async def main():
     """main entrypoint: argparses, delegates to test scaffold, suite generator, gradescope output"""
     if not sys.argv:
@@ -178,8 +188,10 @@ async def main():
             tests = generate_test_suite_v2()
         case "3":
             tests = generate_test_suite_v3()
+        case "4":
+            tests = generate_test_suite_v4()    
         case _:
-            raise ValueError("Unsupported version; expect one of {1, 2}")
+            raise ValueError("Unsupported version; expect one of {1, 2, 3, 4}")
 
     results = await run_all_tests(scaffold, tests)
     total_score = get_score(results) / len(results) * 100.0
